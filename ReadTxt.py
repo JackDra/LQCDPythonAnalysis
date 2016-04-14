ede95c892f5e4bf9b9866cc0df4def9cfbdae48e
Jack Dragos <jack.dragos@gmail.com>
2016-04-14 10:18:52 +0930

    autocommit

--- ReadTxt.py
+++ ReadTxt.py
@@ -281,8 +281,8 @@ def RewriteRF(RFdict,threeptdict,thisopp,thismom):
             if CheckDict(RFdict,igamma,thismom,'RF'):
                 for iset in RFdict[igamma][thismom]['RF'].keys():
                     print igamma , iset
-                    for it,(i3pt,i3ptopp) in enumerate(zip(threeptdict[igamma][thismom]['RF'][iset]['Boot'],
-                                                           threeptdict[thisopp][thismom]['RF'][iset]['Boot'])):
+                    for it,(i3pt,i3ptopp) in enumerate(zip(threeptdict[igamma][thismom]['RF'][iset]['Boot'][tsource-1:GetintTSink(iset)],
+                                                           threeptdict[thisopp][thismom]['RF'][iset]['Boot'][tsource-1:GetintTSink(iset)])):
                         RFdict[igamma][thismom]['RF'][iset]['Boot'][it].values = i3pt.values/i3ptopp.values
                         GetBootStats(RFdict[igamma][thismom]['RF'][iset]['Boot'][it])
                         RFdict[igamma][thismom]['RF'][iset]['Vals'][it] = RFdict[igamma][thismom]['RF'][iset]['Boot'][it].Avg
