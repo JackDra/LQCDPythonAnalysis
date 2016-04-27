ae124529d7f8013c169f9872fcfeddba35059f30
Jack Dragos <jack.dragos@gmail.com>
2016-04-27 15:18:48 +0930

    autocommit

--- Params.py
+++ Params.py
@@ -211,7 +211,7 @@ for ito in DeftoList:
         TwoPtDefTvarList.append('to'+str(ito)+'dt'+str(idt))
         TwoTotDefTvarList.append('PoF'+str(PoFShifts)+'to'+str(ito)+'dt'+str(idt))
 
-TwoTotDefTvarList = DefTvarList+TwoTotDefTvarList
+TwoTotDefTvarList += DefTvarList
 DefTvarto16 = []
 DefTvarto17 = []
 DefTvarto18 = []
