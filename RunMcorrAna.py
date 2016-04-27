bb7c830cededfaee67db73f589a799285b597ab9
Jack Dragos <jack.dragos@gmail.com>
2016-04-27 15:43:02 +0930

    autocommit

--- RunMcorrAna.py
+++ RunMcorrAna.py
@@ -111,9 +111,9 @@ def RunOffCorrs(thisPool,Curr,RunType,RunTSinkList=None,WipeThisSet=False):
                 iPrefList = [thisPrefList[it]]*len(itsinkList)
             else:
                 itsinkList,iPrefList = [itsink],[thisPrefList[it]]
-            thisSetList,this2ptSetList,dump = CreateSet(thisSmearL=wipeSL,thisSingSmearL=wipeSL,thisTvarL=thisTvarList,thisTSinkL=map(str,itsinkList),
-                                                        thisREvecTvarL=thisREvecTvarList,thisREvecTSinkL=map(str,itsinkList),
-                                                        thisPoFTvarL=thisPoFTvarList,thisPoFTSinkL=map(str,itsinkList))
+            thisSetList,this2ptSetList,dump = CreateSet(thisSmearL=wipeSL,thisSingSmearL=wipeSL,thisTvarL=thisTvarList,thisTSinkL=[str(itsink)],
+                                                        thisREvecTvarL=thisREvecTvarList,thisREvecTSinkL=[str(itsink)],
+                                                        thisPoFTvarL=thisPoFTvarList,thisPoFTSinkL=[str(itsink)])
             print thisSetList
             if 'giDi' == Curr:
                 if WipeThisSet:
