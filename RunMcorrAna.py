ac0b9ab82dbbefb906388e8e8d58eff2dee97b74
Jack Dragos <jack.dragos@gmail.com>
2016-04-27 14:58:28 +0930

    autocommit

--- RunMcorrAna.py
+++ RunMcorrAna.py
@@ -97,7 +97,7 @@ def RunOffCorrs(thisPool,Curr,RunType,RunTSinkList=None,WipeThisSet=False,DoPoF=
     print '----------------------------------------------------------------------------------'
     if RunType == 'TwoPt':
         print 'Two Point Analysis'
-        Wipe2pt(outputdir,statelist=StateSet,todtlist=DefTvarList,smlist=DefSmearList,DoPoF=DoPoF)
+        Wipe2pt(outputdir,statelist=StateSet,todtlist=TwoPtDefTvarList,smlist=DefSmearList,DoPoF=DoPoF)
         thisMomList = Get2ptSetMoms(outputdir,RunMomList,statelist=StateSet,todtlist=DefTvarList,smlist=DefSmearList)
         CreateTwoPt(DragpZ([qstrTOip(iq) for iq in thisMomList]),DefSmearList,DoPoF=DoPoF)
         print 'Two Point Analysis Complete'
