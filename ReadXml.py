344c50473741e9e0c3181a15f52abb4a5672060f
Jack Dragos <jack.dragos@gmail.com>
2016-04-28 17:52:39 +0930

    autocommit

--- ReadXml.py
+++ ReadXml.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
 import xmltodict
+from XmlFuns import *
 
+def ReadMyXml(filein):
+    with open(filein+'.xml','r') as f:
+        data = RecursiveFTDASxmltodict.parse(f.read()))
+    return data
 
--- XmlFuns.py
+++ XmlFuns.py
@@ -13,10 +13,10 @@ def BootAvgStdToFormat(Dict,frmtflag='f'):
     return AvgStdToFormat(Dict.Avg,Dict.Std,frmtflag=frmtflag)
 
 def FormatToAvgStd(String):
-    return String.strip().split()
+    return map(float,String.strip().split())
 
 def FormatToDictAvgStd(String):
-    return {zip(['Avg','Std'],FormatToAvgStd(String))}
+    return dict(zip(['Avg','Std'],FormatToAvgStd(String)))
 
 
 def RecursiveFTDAS(dictin):
@@ -25,8 +25,12 @@ def RecursiveFTDAS(dictin):
             dictin[ikey] = RecursiveFTDAS(idata)
         return dictin
     else:
-        print dictin
-        if isinstance(dictin,str):
-            return FormatToDictAvgStr(dictin)
-        else:
-            raise TypeError('final value in dictionary is not string')
+        try:
+            dictout = map(float,dictin)
+        except:
+            try:
+                dictout = FormatToDictAvgStd(str(dictin))
+            except:
+                raise TypeError('final value in dictionary is not string')
+        return dictout
+
