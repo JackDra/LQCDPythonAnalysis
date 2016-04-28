6afd33344b96e0215bd823125a6bc7d2eb33f449
Jack Dragos <jack.dragos@gmail.com>
2016-04-28 15:12:57 +0930

    autocommit

--- xmltodict.py
+++ xmltodict.py
@@ -343,6 +343,7 @@ def _emit(key, value, content_handler,
         attrs = OrderedDict()
         children = []
         for ik, iv in v.items():
+            ik = str(ik)
             if ik == cdata_key:
                 cdata = iv
                 continue
