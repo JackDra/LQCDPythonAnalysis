e446817b9e13d118b0db56394cf18ba3855807d5
Jack Dragos <jack.dragos@gmail.com>
2016-04-28 20:50:43 +0930

    autocommit

--- OutputXmlData.py
+++ OutputXmlData.py
@@ -33,6 +33,7 @@ def PrintFitToFile(data,dataChi,iset,filename,thisMomList,thisCutList):
     datadict = {'Fits':{'Values':OrderedDict(),'Boots':OrderedDict()}}
     xmlMomList = map(ipTOqcond,thisMomList)
     xmlCutList = map(xmlcut,thisCutList)
+    print ''
     print len(data),len(dataChi),len(xmlMomList)
     for ip,pdata,pdataChi in zip(xmlMomList,data,dataChi):        
         datadict['Fits']['Values'][ip] = OrderedDict()
