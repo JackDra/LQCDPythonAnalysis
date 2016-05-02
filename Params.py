863f1004bfe71b7e26cb95649ad5956bddd954af
Jack Dragos <jack.dragos@gmail.com>
2016-05-02 10:38:58 +0930

    autocommit

--- Params.py
+++ Params.py
@@ -109,7 +109,7 @@ pickledir = datadir+"pickledir/"
 REvecDir = scriptdir+'REvecSave/k'+str(kappa)+'/'
 # RunMomList = qvecSet 
 # For Debuggin, only use zero momenta
-RunMomList = [qvecSet[iqTOip(0)]]
+RunMomList = [qvecSet[iqTOip(0)],qvecSet[qvecTOip([0,0,1])]]
 mkdir_p(outputdir)
 mkdir_p(pickledir)
 mkdir_p(logdir)
