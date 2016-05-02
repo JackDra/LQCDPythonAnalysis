fb20a6ddc48fa103aa7ae88ccd336b81960024cb
Jack Dragos <jack.dragos@gmail.com>
2016-05-02 15:51:32 +0930

    autocommit

--- MiscFuns.py
+++ MiscFuns.py
@@ -19,10 +19,10 @@ def merge_dicts(a, b, path=None):
       if key in a:
          if isinstance(a[key], dict) and isinstance(b[key], dict):
             merge_dicts(a[key], b[key], path + [str(key)])
-         elif a[key] == b[key]:
-            pass # same leaf value
+         # elif a[key] == b[key]:
+         #    pass # same leaf value
          else:
-            a[key] = b[key]
+            pass
             # raise Exception('Conflict at %s' % '.'.join(path + [str(key)]))
       else:
          a[key] = b[key]
