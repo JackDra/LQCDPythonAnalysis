346e137dd85098897fb494f7a3dbe0e773dd3fe4
Jack Dragos <jack.dragos@gmail.com>
2016-05-06 09:58:12 +0930

    autocommit

--- TryOneStateFit.py
+++ TryOneStateFit.py
@@ -160,9 +160,8 @@ print 'reading 2 point correlator data'
 print 'reading 2 point correlators finished'
 print ''
 
-print ReadGammaList
 for igamma in ReadGammaList:
-    if 'doub' not in igamma or 'sing' not in igamma:
+    if 'doub' not in igamma and 'sing' not in igamma:
         print 'Running ' + igamma
         DoOSF(ReadSetList,[igamma,'doub'+igamma,'sing'+igamma],data2pt,twoptGammaMomList)
     elif igamma.replace('doub','').replace('sing','') not in ReadGammaList:
