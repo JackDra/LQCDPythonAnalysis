aa5763af785707a3ed953b0778bec16897ddf416
Jack Dragos <jack.dragos@gmail.com>
2016-05-06 18:34:03 +0930

    autocommit

--- ReadXml.py
+++ ReadXml.py
@@ -20,12 +20,11 @@ def ReadXmlDict(filein):
     print xmldata
     firstkey = xmldata.keys()[0]
     if firstkey != 'Null':
-        # try:
-        with open(xmldata[firstkey]['Boots'] , "rb") as pfile:
-            readdata =  pickle.load( pfile )
-            xmldata[firstkey]['Boots'] = readdata
-        # except:
-        #     print 'Reading pickle file fail: ' + xmldata[firstkey]['Boots']
+        try:
+            with open(xmldata[firstkey]['Boots'] , "rb") as pfile:
+                xmldata[firstkey]['Boots'] = pickle.load( pfile )
+        except:
+            print 'Reading pickle file fail: ' + xmldata[firstkey]['Boots']
     return xmldata
 
     
