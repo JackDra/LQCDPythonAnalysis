07c4bdce1c76389d5adb0221e1ddef3f019ca2df
Jack Dragos <jack.dragos@gmail.com>
2016-05-06 18:37:05 +0930

    autocommit

--- Fitting.py
+++ Fitting.py
@@ -333,6 +333,7 @@ def OneStateFit3pt(fitBoot2pt,C3pt,this3ptCutList,thistsink):
     fitAvg3pt = []
     fitAvg3ptChi = []
     AvgPars  = Pullflag(fitBoot2pt,'Avg') 
+    print fitBoot2pt
     BootPars = np.rollaxis(Pullflag(fitBoot2pt,'values'),1)
     for icut,cutval in enumerate(this3ptCutList):
         tdata3pt = []
