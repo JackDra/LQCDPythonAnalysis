c423dbbfbbe53c16c367104e6f7f496daabad3ed
Jack Dragos <jack.dragos@gmail.com>
2016-05-09 20:30:25 +0930

    autocommit

--- GraphDataNew.py
+++ GraphDataNew.py
@@ -418,7 +418,7 @@ def PlotSummedRF(data,thisfitr):
     for icut,cutdata in data.iteritems():
         print cutdata.keys()
         print cutdata['fit con '+thisfitr].keys()
-        print cutdata['fit con '+thisfitr]['Boots'].keys()
+        print cutdata['fit con '+thisfitr]['Boot']
         tdata,dataplot,dataploterr = [],[],[]
         thiscol,thissym = thiscolcyc.next(),thissymcyc.next()
         
