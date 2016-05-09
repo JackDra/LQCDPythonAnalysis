9a3677f0ff0769139bad913c9cfe82e3dcef3860
Jack Dragos <jack.dragos@gmail.com>
2016-05-09 20:21:54 +0930

    autocommit

--- MiscFuns.py
+++ MiscFuns.py
@@ -265,7 +265,7 @@ def NDimOpp3(data,data2,data3,dimleft,funin,*funvars):
 def Pullflag(data,atribute):
     npdata = np.array(data)
     dataout = np.array([])
-    if len(npdata.flatten()) == 0: return getattr(data,atribute)
+    if len(npdata.flatten()) == 0: return []
     for idata in npdata.flatten():
         flagdim = np.array(getattr(idata,atribute)).shape
         dataout = np.append(dataout,getattr(idata,atribute))
