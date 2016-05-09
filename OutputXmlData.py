804c1565231a6b8d8ee0f4e189c791017b5de805
Jack Dragos <jack.dragos@gmail.com>
2016-05-09 16:32:26 +0930

    autocommit

--- OutputXmlData.py
+++ OutputXmlData.py
@@ -37,9 +37,10 @@ def WriteXmlOutput(thisfile,outputdict):
     
         
 def MergeXmlOutput(thisfile,outputdict):
-    if CheckMomFile(thisfile+'.xml'):
-        thisdict = ReadXmlAndPickle(thisfile+'.xml')[0]
-        outputdict = merge_dicts(outputdict,thisdict)
+    if os.path.isfile(thisfile+'.xml'):
+        if CheckMomFile(thisfile+'.xml'):
+            thisdict = ReadXmlAndPickle(thisfile+'.xml')[0]
+            outputdict = merge_dicts(outputdict,thisdict)
     if len(outputdict.keys()) > 1:
         raise IOError('Xml main key not single:' + ','.join(outputdict.keys()))
     WriteXmlOutput(thisfile,outputdict)
