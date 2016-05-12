24443a9abfe8d8ee9fc38a188c7177707cf66250
Jack Dragos <jack.dragos@gmail.com>
2016-05-12 17:31:23 +0930

    autocommit

--- TryFits.py
+++ TryFits.py
@@ -79,14 +79,15 @@ totstart = time.time()
 inputparams = []
 nchunk = 1
 RunGammaList = []
-for igamma in thisGammaList:
-    if 'doub' not in igamma and 'sing' not in igamma:
-        for iChunk,(iSetChunk,iTSChunks) in enumerate(zip(chunks(feedin['set'],nchunk),chunks(DefTSinkSetList,nchunk))):
-            if Check3ptFiles(['doub'+igamma,'sing'+igamma,igamma],iSetChunk,feedin['mom'],CheckType='Fits'):
-                print igamma , ' present for ' ,  ','.join(iSetChunk) 
-            else:
-                RunGammaList.append(igamma)
-                inputparams.append((['doub'+igamma,'sing'+igamma,igamma],iSetChunk,feedin['mom'],iTSChunks,(iChunk*100)/float(len(chunks(feedin['set'],nchunk)))))
+for imom in feedin['mom']:
+    for igamma in thisGammaList:
+        if 'doub' not in igamma and 'sing' not in igamma:
+            for iChunk,(iSetChunk,iTSChunks) in enumerate(zip(chunks(feedin['set'],nchunk),chunks(DefTSinkSetList,nchunk))):
+                if Check3ptFiles(['doub'+igamma,'sing'+igamma,igamma],iSetChunk,[imom],CheckType='Fits'):
+                    print igamma , ' present for ' ,  ','.join(iSetChunk) , imom
+                else:
+                    RunGammaList.append(igamma)
+                    inputparams.append((['doub'+igamma,'sing'+igamma,igamma],iSetChunk,[imom],iTSChunks,(iChunk*100)/float(len(chunks(feedin['set'],nchunk)))))
 
 if DoMulticore:
     makeContextFunctions(TryFitsFun)
