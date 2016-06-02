d07202846f916cf7054e2df01cc8bb7409096762
Jack Dragos <jack.dragos@gmail.com>
2016-06-02 18:13:45 +0930

    autocommit

--- TrySummation.py
+++ TrySummation.py
@@ -62,14 +62,23 @@ inputparams = []
 for igamma in thisGammaList:
     if 'doub' not in igamma and 'sing' not in igamma and 'twopt' not in igamma:
         parsegammalist = ['doub'+igamma,'sing'+igamma,igamma]
-        QueMomList = Check3ptAllSets(parsegammalist,thisReadSetList,thisMomList=feedin['mom'],CheckType='SumMeth')
-        for imom in QueMomList[igamma]:
+        if DefWipe:
+            QueMomList = feedin['mom']
+        else:
+            QueMomList = Check3ptAllSets(parsegammalist,thisReadSetList,thisMomList=feedin['mom'],CheckType='SumMeth')
+            QueMomList = QueMomList[igamma]
+        for imom in QueMomList:
             print 'adding to que: ' , igamma , imom 
             inputparams.append((parsegammalist,thisReadSetList,thisTSinkList,thisReadSet2pt,[imom]))
     elif igamma.replace('sing','').replace('doub','') not in thisGammaList and 'twopt' not in igamma:
         parsegammalist = [igamma]
-        QueMomList = Check3ptAllSets(parsegammalist,thisReadSetList,thisMomList=feedin['mom'],CheckType='SumMeth')
-        for imom in QueMomList[igamma]:
+        
+        if DefWipe:
+            QueMomList = feedin['mom']
+        else:
+            QueMomList = Check3ptAllSets(parsegammalist,thisReadSetList,thisMomList=feedin['mom'],CheckType='SumMeth')
+            QueMomList = QueMomList[igamma]
+        for imom in QueMomList:
             print 'adding to que: ' , igamma , imom
             inputparams.append((parsegammalist,thisReadSetList,thisTSinkList,thisReadSet2pt,[imom]))
 makeContextFunctions(FitSumWrap)
