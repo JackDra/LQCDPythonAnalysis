ff9de9332fb6c8528f82fbcf6349b199c089328e
Jack Dragos <jack.dragos@gmail.com>
2016-06-02 17:57:17 +0930

    autocommit

--- ReadXml.py
+++ ReadXml.py
@@ -68,7 +68,9 @@ def ReadRFFile(filedir,filename,thisMomList=RunMomList):
             if 'Boots' in data.keys():
                 bootdata = data['Boots']
                 dictout[thismom] = {}
-                if 'Info' in data.keys():dictout[thismom]['Info'] = data['Info']
+                if 'Info' in data.keys():
+                    dictout[thismom]['Info'] = data['Info']
+                    print dictout[thismom]['Info']
                 dictout[thismom]['tVals'] = map(untstr,bootdata.keys())
                 dictout[thismom]['Boot'] = []
                 dictout[thismom]['Vals'] = []
