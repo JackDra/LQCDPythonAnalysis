d0eb7af19948726f6b7f808ecb7544ccf1e5a6e7
Jack Dragos <jack.dragos@gmail.com>
2016-06-14 21:19:55 +0930

    autocommit

--- TryNconfs.py
+++ TryNconfs.py
@@ -30,9 +30,10 @@ ShowSetLists(feedin['set'])
 
 ShowMethodList(feedin['method'])
 
-nconf = CheckNconf(thisGammaList,feedin['set'],thisMomList=feedin['mom'],CheckList=feedin['method'])
+for iset in feedin['set']:
+    nconf = CheckNconf(thisGammaList,feedin['set'],thisMomList=feedin['mom'],CheckList=feedin['method'])
 
-if isinstance(nconf, int):
-    print 'Smallest number of configurations used is', nconf
-else:
-    print nconf
+    if isinstance(nconf, int):
+        print iset , 'Smallest number of configurations used is', nconf
+    else:
+        print nconf
