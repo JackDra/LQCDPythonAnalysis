2db38ace6991fc0096b2d6706cc3b2dce5830251
Jack Dragos <jack.dragos@gmail.com>
2016-06-14 17:28:55 +0930

    autocommit

---
+++ TryNconfs.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+
+from OppFuns import CreateGammaList,WipeSet
+from Params import *
+import numpy as np
+from BootTest import BootStrap1
+from ReadTxt import *
+from SetLists import *
+from Fitting import FitRFSet
+from OutputData import PrintFitSetToFile
+from FitParams import *
+from multiprocessing import Pool
+from MultiWrap import *
+from FFParams import *
+import itertools as it
+import sys
+import time
+import datetime
+from InputArgs import *
+from CheckXml import *
+
+
+
+
+
+feedin = InputParams(sys.argv[1:])
+DefWipeWarning()
+thisGammaList = CreateGammaList(feedin['gamma'])
+
+ShowSetLists(feedin['set'])
+
+
+nconf = CheckNconf(thisGammaList,feedin['set'],thisMomList=feedin['mom'],CheckList=feedin['method'])
+
+print nconf
