83e4fa371c6dc29fddfd22357790fb0e8e488bfe
Jack Dragos <jack.dragos@gmail.com>
2016-06-14 17:11:31 +0930

    autocommit

---
+++ CheckNconfigs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+
+from InputArgs import *
+from CheckXml import CheckNconf
+from OppFuns import CreateGammaList
+
+
+feedin = InputParams(sys.argv[1:])
+DefWipeWarning()
+thisGammaList = CreateGammaList(feedin['gamma'])
+
+ShowSetLists(feedin['set'])
+
+Nconf = CheckNconf(thisGammaList,feedin['set'],thisMomList=feedin['mom'],CheckList=feedin['method'],cfuns=False)
+
+print Nconf
