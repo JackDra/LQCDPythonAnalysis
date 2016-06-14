00a7c35031a4abe3f5e4180edf7092bffdfc7ba1
Jack Dragos <jack.dragos@gmail.com>
2016-06-14 17:25:10 +0930

    autocommit

--- CheckNconfigs.py
+++ CheckNconfigs.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
 
-
+from OppFuns import CreateGammaList,WipeSet
 from Params import *
-from OppFuns import *
+import numpy as np
+from BootTest import BootStrap1
+from ReadTxt import *
+from SetLists import *
+from Fitting import FitRFSet
+from OutputData import PrintFitSetToFile
+from FitParams import *
+from multiprocessing import Pool
+from MultiWrap import *
+from FFParams import *
+import itertools as it
+import sys
+import time
+import datetime
 from InputArgs import *
 from CheckXml import *
-
 
 feedin = InputParams(sys.argv[1:])
 DefWipeWarning()
