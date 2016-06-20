186e6fedbc87badd6562e3708a912a2bc6df5bb2
Jack Dragos <jack.dragos@gmail.com>
2016-06-20 14:11:14 +0930

    autocommit

--- CMSTech.py
+++ CMSTech.py
@@ -122,6 +122,12 @@ def CreateLREves(Cfunto,Cfuntodt,thisdt,masscutoff):
         ThisMat = ShalfInv.dot(Simtodt[ci[:,None],ci].dot(ShalfInv))
         thiseig,thisevec = eigh(ThisMat)
         evecreal,evecimag = SplitCmplxReal(thisevec.flatten())
+        print thiseig
+        print evecreal
+        print evecimag
+        print evecreal < 0
+        print map(abs,evecimag) > 0
+        print -np.log(map(abs,thiseig))/float(thisdt) < VarMassCutoff
         if any(-np.log(map(abs,thiseig))/float(thisdt) < VarMassCutoff) or any(evecreal < 0) or any(map(abs,evecimag) > 0):
             # ibad = [ie < 0 for ie in thiseig].index(True)
             ci = np.delete(ci,ci.tolist().index(cutindex))
