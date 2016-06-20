383a55f41745d6bdf1b5a054ba0a10d1066924b6
Jack Dragos <jack.dragos@gmail.com>
2016-06-20 13:17:57 +0930

    autocommit

--- CMSTech.py
+++ CMSTech.py
@@ -144,12 +144,12 @@ def CreateLREves(Cfunto,Cfuntodt,thisdt,masscutoff):
         Simtodt = np.array(Cfuntodt)[ci[:,None],ci]
         ShalfInv = inv(sqrtm(Simto))
         ThisMat = ShalfInv.dot(Simtodt.dot(ShalfInv))
-        [Evals,REvec] = eigh(ThisMat)
-        # [Evals,LEvec,REvec] = eig(ThisMat,left=True)
+        # [Evals,REvec] = eigh(ThisMat)
+        [Evals,LEvec,REvec] = eig(ThisMat,left=True)
         ## w = G^-1/2 u
-        # LEvec = LEvec.dot(ShalfInv)
+        LEvec = LEvec.dot(ShalfInv)
         REvec = ShalfInv.dot(REvec)
-        LEvec = REvec
+        # LEvec = REvec
         Evals,LEvec,REvec = AddNullState(Evals,LEvec,REvec,buffindex,thisdt=thisdt)
     return sortEvec(Evals,LEvec,REvec,thisdt)
 
