c95975eb81bee355c4667d7b01dea4a49657bbf1
Jack Dragos <jack.dragos@gmail.com>
2016-06-22 09:54:49 +0930

    autocommit

--- TryNconfs.py
+++ TryNconfs.py
@@ -9,9 +9,10 @@ from CheckXml import *
 from OutputXmlData import WriteXml
 
 minmax = 'max'
-if sys.argv[1] == 'min' or sys.argv[1] == 'max':
-    minmax = sys.argv[1]
-    sys.argv = sys.argv[0] + sys.argv[2:]
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'min' or sys.argv[1] == 'max':
+        minmax = sys.argv[1]
+        sys.argv = [sys.argv[0]] + sys.argv[2:]
         
 feedin = InputParams(sys.argv[1:] + ['-noprompt'])
 thisGammaList = CreateGammaList(feedin['gamma'])
