868e73f01f5b3fb161ab65c436cdb773a7e3614e
Jack Dragos <jack.dragos@gmail.com>
2016-06-23 16:04:35 +0930

    autocommit

--- ReadXml.py
+++ ReadXml.py
@@ -66,7 +66,7 @@ def CheckMomFile(filein,nconftest = False):
     if nconftest != False:
         filenconf = CheckNconfFile(filein)
         mprint(filein)
-        mprint('has nconf = ' + filenconf)
+        mprint('has nconf = ' + str(filenconf))
         if nconftest == filenconf:
             return True
         else:
