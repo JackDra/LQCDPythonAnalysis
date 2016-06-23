8354d776450a7123d0cf012775e4f3606e0cd88e
Jack Dragos <jack.dragos@gmail.com>
2016-06-23 15:35:12 +0930

    autocommit

--- CheckXml.py
+++ CheckXml.py
@@ -136,6 +136,7 @@ def Check3ptArray(thisGammaList,thisSetList,thisMomList=RunMomList,CheckType='',
                     for dsgamma in ['doub'+igamma,'sing'+igamma]:
                         filename = iset+dsgamma
                         gammadir = thisdir+CreateOppDir(dsgamma)+'/' + CheckType
+                        print 'DEBUG'
                         CheckBool = CheckBool and all([CheckMomFile(SetUpPDict(ip,gammadir,filename+iSF)[1]+'.xml',nconftest=thisNconf) for iSF in SFList])
                 else:
                     filename = iset+igamma
