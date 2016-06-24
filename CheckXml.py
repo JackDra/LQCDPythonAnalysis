a3b41b136309f1190c39c11a5e69098e5b4b1f21
Jack Dragos <jack.dragos@gmail.com>
2016-06-24 20:42:27 +0930

    autocommit

--- CheckXml.py
+++ CheckXml.py
@@ -160,6 +160,7 @@ def CheckNconf(inputGammaList,CheckSetList,thisMomList=RunMomList,CheckList=['']
                                 elif minmax == 'max':
                                     nconf = max(nconf, thisnconf)
     print ' '*50
+    print massnconf
     if isinstance(massnconf,int) :
         if minmax == 'min':
             nconf = min(nconf, massnconf)
