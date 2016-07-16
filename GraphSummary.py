8933aa662d45b8c1e68a496460e79d55bd0e149b
Jack Dragos <jack.dragos@gmail.com>
2016-07-16 09:31:19 +0930

    autocommit

--- GraphSummary.py
+++ GraphSummary.py
@@ -232,12 +232,8 @@ def PlotSummarySet(iDS,igamma,iq,data,thisMeth,thisSetList,xstart,col,sym):
             if 'Fits' in thisMeth: methcomp = 'Fits'
             if iSet in datakey.replace(methcomp,'') and methcomp in datakey: 
                 if iDS == False:
-                    if Debug:
-                        print idata.keys() , igamma
-                        print idata[igamma].keys() , iq
-                        print GraphCondit('',igamma,iq,methcomp,iSet)
                     if not CheckDict(idata,igamma,iq): continue
-                    if not GraphCondit('',iSet,igamma,iq,methcomp,iSet): continue
+                    if not GraphCondit('',igamma,iq,methcomp,iSet): continue
                     Xlables.append(LabToXaxis(iSet,thisMeth))
                     if 'Avg' in idata[igamma][iq].keys() and 'Std' in idata[igamma][iq].keys():
                         dataval.append(abs(idata[igamma][iq]['Avg']))
