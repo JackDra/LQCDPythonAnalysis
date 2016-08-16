9e9c600e3d9f4274f70220907ec1491c120e806e
Jack Dragos <jack.dragos@gmail.com>
2016-08-16 13:56:50 +0930

    autocommit

--- Params.py
+++ Params.py
@@ -227,9 +227,11 @@ DefdtList = [1,2,3,4]
 ##MUST BE IN SORTING ORDER##
 # DeftodtPicked = [(18,2),(20,2)]
 # DefTvarPicked = ['CMto'+str(iDeftodtPicked[0])+'dt'+str(iDeftodtPicked[1]) for iDeftodtPicked in DeftodtPicked]
-
-DeftodtPicked = Elongate(DeftoList,DefdtList)
+DeftodtPicked = [(20,2)]
 DefTvarPicked = ['CMto'+str(iDeftodtPicked[0])+'dt'+str(iDeftodtPicked[1]) for iDeftodtPicked in DeftodtPicked]
+
+# DeftodtPicked = Elongate(DeftoList,DefdtList)
+# DefTvarPicked = ['CMto'+str(iDeftodtPicked[0])+'dt'+str(iDeftodtPicked[1]) for iDeftodtPicked in DeftodtPicked]
 
 DeftodtList = []
 DefTvarList = []
