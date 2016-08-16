fda6a4067e2daf03d25f6a62d99699d6638824dd
Jack Dragos <jack.dragos@gmail.com>
2016-08-17 00:25:42 +0930

    autocommit

--- FitParams.py
+++ FitParams.py
@@ -59,8 +59,8 @@ OneStateParList = {'C2':['Am','m0'] , 'C3':['B00']}
 #picked for plotting
 OSFCutList = ['cut4','cut5']
 OSFCutPicked = 'cut5'
-OSFfitvals = {'sm32': [28,33], 'sm64': [27,33] , 'sm128':[26,33],
-              PickedStateStr+'REvec':[24,33],PickedStateStr+'PoF'+str(PoFShifts):[18,33]}
+OSFfitvals = {'sm32': [29,35], 'sm64': [28,35] , 'sm128':[27,35],
+              PickedStateStr+'REvec':[25,35],PickedStateStr+'PoF'+str(PoFShifts):[18,35]}
     
 OSFfitr = {'sm32':str(OSFfitvals['sm32'][0])+'-'+str(OSFfitvals['sm32'][1]),
            'sm64':str(OSFfitvals['sm64'][0])+'-'+str(OSFfitvals['sm64'][1]),
