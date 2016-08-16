359dd2a46b9366ffb2080be01b9f7c29a7ab3434
Jack Dragos <jack.dragos@gmail.com>
2016-08-16 11:45:06 +0930

    autocommit

--- GraphDataNew.py
+++ GraphDataNew.py
@@ -297,6 +297,7 @@ def PlotRFSet(data,thisSetList,legrem='',MassDt = False):
             dataplot = deepcopy(data['RF'][iset])
             dataplot['Boot'] = MassFun(dataplot['Boot'],MassDt)
             # dataplot['tVals'] = dataplot['tVals'][:-MassDt] 
+            dataplot['tVals'] = dataplot['tVals'][MassDt:] 
             PlotRF(dataplot,thiscolcyc.next(),thissymcyc.next(),thisshiftcyc.next(),LegLab(iset),MP=True)
 
 def PlotLogSet(data,thisSetList,legrem=''):
