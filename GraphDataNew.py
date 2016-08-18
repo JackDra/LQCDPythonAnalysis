0783e1f78324167f9879cfae4eea9922b0008a4f
Jack Dragos <jack.dragos@gmail.com>
2016-08-18 15:02:53 +0930

    autocommit

--- GraphDataNew.py
+++ GraphDataNew.py
@@ -193,7 +193,7 @@ def PlotTSinkSumData(data,thisSetList,thisGamma,thisMom,thissm='sm32'):
         pl.subplot(1,len(SumFitRList),ic+1)
         PlotColSumFun(data,thisSetList,[thissm],thisGamma,thisMom,'Sum TSink Comparison ',thisTsinkR=ifitr)
         SetSumFunAxies(ic==0)
-    pl.gcf().subplots_adjust(top=0.85)
+    pl.subplots_adjust(top=0.85)
     pl.savefig(CreateFile(thissm,thisGamma,thisMom,'Sum TSink Comparison ',suptitle=True)+'Sfun.pdf')
     pl.clf()
         
