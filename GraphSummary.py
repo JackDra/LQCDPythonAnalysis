cfb34da6dd089799f4612fedd2c57cd8f052aee7
Jack Dragos <jack.dragos@gmail.com>
2016-08-18 23:43:15 +0930

    autocommit

--- GraphSummary.py
+++ GraphSummary.py
@@ -59,7 +59,7 @@ def GraphCondit(iDS,igamma,iq,thisMeth,iSet):
     # if 'OSF' in thisMeth and 'tsink26sm32' in iSet: graphthis = False
     if 'Fits' in thisMeth and 'tsink32' in iSet and 'to18dt2' in iSet: graphthis = False
     if 'Fits' in thisMeth and 'tsink26' in iSet and 'to18dt2' in iSet: graphthis = False
-    if 'SumMeth' in thisMeth and 'fitr2-4' in iSet: graphthis = False
+    # if 'SumMeth' in thisMeth and 'fitr2-4' in iSet: graphthis = False
     return graphthis
 
 ##FIX STRINGS tsink and sm stuff##
