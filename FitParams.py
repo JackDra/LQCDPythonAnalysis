255c6c051f5fa0af8dad4443acb5af1c6def93e3
Jack Dragos <jack.dragos@gmail.com>
2016-08-19 19:44:52 +0930

    autocommit

--- FitParams.py
+++ FitParams.py
@@ -26,8 +26,8 @@ FitCutArgs = ['cut'+str(ic) for ic in FitCutList]
 ##TryFits.py
 FitCutPicked =  {'tsink26sm32':'cut6',
                  'tsink26'+PickedStateStr+'REvec':'cut4',
-                 'tsink29sm32':'cut6',
-                 'tsink29sm64':'cut6',
+                 'tsink29sm32':'cut5',
+                 'tsink29sm64':'cut5',
                  'tsink29sm128':'cut5',
                  'tsink29'+PickedStateStr+'CM':'cut5',
                  'tsink32sm32':'cut6',
