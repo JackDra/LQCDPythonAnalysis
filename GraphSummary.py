91bb92d27f3fd0eeb3765fdcbae047e0cc888001
Jack Dragos <jack.dragos@gmail.com>
2016-08-19 19:13:27 +0930

    autocommit

--- GraphSummary.py
+++ GraphSummary.py
@@ -104,8 +104,8 @@ def PlotXlabs(method,thissetlist,xminmax):
         line1 = 'Var'
         # line2 = SplitToDt(smlist[0])[0]
         # line3 = [SplitToDt(smlist[0])[1].replace('dt','\Delta t')]
-        line2 = r'\Delta t=2'
-        line3 = []
+        line2 = r'\Delta'
+        line3 = ['t=2']
     elif 'TSF' in method:
         line1 = '2SF'
         if 'CM' in method:
