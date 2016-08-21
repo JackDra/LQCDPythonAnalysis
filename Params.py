39d5f46b6c9f7d83d426071beb589cb9ff439c7d
Jack Dragos <jack.dragos@gmail.com>
2016-08-21 16:45:03 +0930

    autocommit

--- Params.py
+++ Params.py
@@ -312,7 +312,7 @@ AllTSinkStrListNoCM = ['tsink'+str(its) for its in AllTSinkListNoCM]
 
 # AllREvecTSinkList = {'12104':[29],'12090':[32]}
 # AllREvecTSinkList = {'12104':[29],'12090':[26,32]}
-AllREvecTSinkList = {'12104':[29],'12090':[32]}
+AllREvecTSinkList = {'12104':[29],'12090':[]}
 REvecTSinkList = AllREvecTSinkList[str(kappa)]
 REvecTSinkStrList = ['tsink'+str(its) for its in REvecTSinkList]
 DefREvecVarList = [18,2]
