1b0e19d7c3e7bf121cea5c150956b9398e89c9d9
Jack Dragos <jack.dragos@gmail.com>
2016-08-23 18:30:06 +0930

    autocommit

--- FFFuns.py
+++ FFFuns.py
@@ -136,7 +136,7 @@ def RenormFF(FF,Val,thisDS):
             elif 'Proton' in thisDS:
                 FF[Qsqrdkey]['Boot'][icFF] = iFF/Val            
             FF[Qsqrdkey]['Boot'][icFF].Stats()
-            FF[Qsqrdkey]['Avg'][icFF] = FF[Qsqrdkey]['Boot'][icFF].Avg
+            # FF[Qsqrdkey]['Avg'][icFF] = FF[Qsqrdkey]['Boot'][icFF].Avg
     return FF
         
 
