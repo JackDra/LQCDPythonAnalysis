138af824da19550f8600aedfdf11242949b986e9
Jack Dragos <jack.dragos@gmail.com>
2016-08-23 18:33:10 +0930

    autocommit

--- FFFuns.py
+++ FFFuns.py
@@ -127,16 +127,17 @@ def CombineVector(thisFF,thisMass):
 
 def RenormFF(FF,Val,thisDS):
     for Qsqrdkey,FFqsqrd in FF.iteritems():
-        for icFF,iFF in enumerate(FFqsqrd['Boot']):
-            if Debug: print Qsqrdkey, icFF, FF[Qsqrdkey]['Boot'][icFF]
-            if 'doub' in thisDS:
-                FF[Qsqrdkey]['Boot'][icFF] = 2*iFF/Val
-            elif 'sing' in thisDS:
-                FF[Qsqrdkey]['Boot'][icFF] = iFF/Val            
-            elif 'Proton' in thisDS:
-                FF[Qsqrdkey]['Boot'][icFF] = iFF/Val            
-            FF[Qsqrdkey]['Boot'][icFF].Stats()
-            # FF[Qsqrdkey]['Avg'][icFF] = FF[Qsqrdkey]['Boot'][icFF].Avg
+        if 'Boot' in FFqsqrd.keys():
+            for icFF,iFF in enumerate(FFqsqrd['Boot']):
+                if Debug: print Qsqrdkey, icFF, FF[Qsqrdkey]['Boot'][icFF]
+                if 'doub' in thisDS:
+                    FF[Qsqrdkey]['Boot'][icFF] = 2*iFF/Val
+                elif 'sing' in thisDS:
+                    FF[Qsqrdkey]['Boot'][icFF] = iFF/Val            
+                elif 'Proton' in thisDS:
+                    FF[Qsqrdkey]['Boot'][icFF] = iFF/Val            
+                FF[Qsqrdkey]['Boot'][icFF].Stats()
+                # FF[Qsqrdkey]['Avg'][icFF] = FF[Qsqrdkey]['Boot'][icFF].Avg
     return FF
         
 
