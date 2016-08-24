8182ac236aa796ffbacdf99c3fd65d3c9220069c
Jack Dragos <jack.dragos@gmail.com>
2016-08-24 17:56:30 +0930

    autocommit

--- GraphDataNew.py
+++ GraphDataNew.py
@@ -528,6 +528,7 @@ def PlotFFs(data,DSCurr,thisSetList,CollName,FT):
 
 def SkipZeroFF(thisFF,thisset):
     skipzero = False
+    print thisset
     if '2' in thisFF or '3' in thisFF:
         # if any([icheck in thisset for icheck in CheckFFZeroList]):
         skipzero = True
