ce46206b64c7a39a9ed2dcab749048c58c07c824
Jack Dragos <jack.dragos@gmail.com>
2016-08-24 11:52:00 +0930

    autocommit

--- GraphDataNew.py
+++ GraphDataNew.py
@@ -53,7 +53,7 @@ ylimDict = {'IsoVectorP4giDi':[-0.15,-0.05],
             # 'IsoVectorP3g3g5':[-0.9,-1.3], ## PoF
             # 'IsoVectorP3g3g5':[-.95,-1.25], ## Tsink
             # 'IsoVectorP4I':[0.4,1.2], ## Tsink?
-            'IsoVectorP4giDi':[0.0,0.5], ## Reg
+            'IsoVectorP4giDi':[0.32,0.18], ## Reg
             # 'IsoVectorP4giDi':[-0.12,-0.06], ## PoF
             # 'IsoVectorP4giDi':[-0.13,-0.07], ## Tsink
             'IsoVectorP3g1g2':[-1.2,-1.04], ## Reg
