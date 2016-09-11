1ab525016b3dc9562f2e69b208842a54e7537976
Jack Dragos <jack.dragos@gmail.com>
2016-09-12 00:18:30 +0930

    autocommit

--- GraphDataNew.py
+++ GraphDataNew.py
@@ -41,10 +41,10 @@ thisalpha = 0.3
 
 # MassTVals = 16,33
 # MassTVals = 3,25
-MassTVals = 3,30
+MassTVals = 3,25
 # Massyrange = 0.44,0.54
 # Massyrange = 0.40,0.60
-Massyrange = 0.40,0.65
+Massyrange = 0.40,0.61
 Qsqrdxlim = -0.03,1
 
 ylimDict = {'VectorP4giDi':[-0.4,-0.15],
