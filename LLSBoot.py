96b9a7d981723c4141cdebd80f0c021b7104e6b7
Jack Dragos <jack.dragos@gmail.com>
2016-09-22 01:19:27 +0930

    autocommit

--- LLSBoot.py
+++ LLSBoot.py
@@ -20,11 +20,11 @@ def LSCreate(Fun):
         #     xval = xval[0]
         yval = val[-2]
         errval = val[-1]
-        print xval
-        print par
-        print Fun(xval,par)
-        print yval
-        print 
+        # print xval
+        # print par
+        # print Fun(xval,par)
+        # print yval
+        # print 
         return (np.array(Fun(xval,par))-yval)/errval
     return LSFun
 
@@ -35,10 +35,10 @@ def LSDerCreate(FunDer):
         #     xval = xval[0]
         yval = val[-2]
         errval = val[-1]
-        # print xval, par
-        # print FunDer(xval,par)
-        # print errval
-        # print 
+        print xval, par
+        print FunDer(xval,par)
+        print FunDer(xval,par)/errval
+        print 
         return np.transpose(FunDer(xval,par)/errval)
         # return np.transpose(FunDer(xval,par)/errval)
     return LSDerFun
