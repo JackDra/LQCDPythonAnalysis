9014bd7bc68bfea8efe020da6d22d6cca5b9ee7c
Jack Dragos <jack.dragos@gmail.com>
2016-09-22 10:18:33 +0930

    autocommit

--- ReadXml.py
+++ ReadXml.py
@@ -93,6 +93,8 @@ def GetDPFitValue(SearchSet,iFF,thisCurr):
     data = ReadXmlDict(filename)[0]
     thisDPpAvg = []
     thisDPpStd = []
+    if Debug: print data.keys(),'DP_Fits'
+    if Debug: print data[data.keys()[0]].keys(),'Values'
     if iFF in data['DP_Fits']['Values'].keys():
         # if any([SearchSet in iset  for iset in data['DP_Fits']['Values'][iFF]['Fzero'].keys()]):
         if SearchSet in data['DP_Fits']['Values'][iFF]['Fzero'].keys() and SearchSet in data['DP_Fits']['Values'][iFF]['mEM'].keys() :
