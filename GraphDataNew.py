c14eda70bead0146b2e73e9ec19aa933c059cfe0
Jack Dragos <jack.dragos@gmail.com>
2016-09-22 10:48:16 +0930

    autocommit

--- GraphDataNew.py
+++ GraphDataNew.py
@@ -565,6 +565,7 @@ def PlotDPFit(thisset,thisFF,thisCurr,thiscol,qrange):
     fitydataAvg = DPfitfun([fitqdata],Avg)
     fitydataup = np.array([max(iy1,iy2) for iy1,iy2 in zip(DPfitfun([fitqdata],Avg+Err),DPfitfun([fitqdata],np.array([Avg[0]-Err[0],Avg[1]+Err[1]])))])
     if Debug: print DPfitfun([fitqdata],Avg-Err),DPfitfun([fitqdata],np.array([Avg[0]+Err[0],Avg[1]-Err[1]]))
+    Avg,Err = np.array(Avg),np.array(Err)
     fitydatadown = np.array([min(iy1,iy2) for iy1,iy2 in zip(DPfitfun([fitqdata],Avg-Err),DPfitfun([fitqdata],np.array([Avg[0]+Err[0],Avg[1]-Err[1]])))])
     # fitydatadown = np.array([np.min(iy1,iy2) for iy1,iy2 in zip(DPfitfun([fitqdata],Avg-Err),DPfitfun([fitqdata],np.array([Avg[0]+Err[0],Avg[1]-Err[1]])))])
     pl.plot(fitqdata,fitydataAvg,label='mPar='+MakeValAndErr(Avg[1],Err[1]),color=thiscol)
