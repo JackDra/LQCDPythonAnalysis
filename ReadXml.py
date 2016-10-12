31931583eef844801ae9526b66d1d2af794115b8
Jack Dragos <jack.dragos@gmail.com>
2016-10-12 18:33:01 +1030

    autocommit

--- ReadXml.py
+++ ReadXml.py
@@ -89,7 +89,7 @@ def CheckMomFile(filein,nconftest = False):
                     
 def GetDPFitValue(SearchSet,iFF,thisCurr,thiskappa=str(kappa)):
     if thiskappa == None: thiskappa = kappa 
-    FFdir = outputdir.replace(str(kappa),thiskappa) +'/FormFactors/DPfits/'
+    FFdir = outputdir.replace('k'+str(kappa),thiskappa) +'/FormFactors/DPfits/'
     filename = FFdir +thisCurr
     if Debug: print filename+'.xml'
     if not os.path.isfile(filename+'.xml'):
