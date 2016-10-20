93e0df113f09cda50c541c2370477f2dcad655f3
Jack Dragos <jack.dragos@gmail.com>
2016-10-20 17:27:57 +1030

    autocommit

--- Params.py
+++ Params.py
@@ -110,7 +110,7 @@ VarMethodMethod = 'Regular' # for solving the Variational method, different ways
 # VarMethodMethod = 'Symmetric'
 # VarMethodMethod = 'AxBxlSolve'
 NoSFRfacScale = False # Turn on to only scale the R function by sqrt((Epp+m)(Ep+m)/EppEp) for form factor creation
-ReadPoF2pt = True # Create PoF using already calculated eigenvectors. This is used if the statistics or solver method has changed.
+ReadPoF2pt = False # Create PoF using already calculated eigenvectors. This is used if the statistics or solver method has changed.
 DeCorrPoF = False ## used for debugging the pencil of function method (decorrelation problem) !!!!!DEPRECIATED, LEAVE FALSE!!!!!
 TimeInv = False ## uses time invariance to calculate the Pencil of Function method/ Oposed to calculating [tsource,tsource-1,...,tsource-PoFShifts]
 
