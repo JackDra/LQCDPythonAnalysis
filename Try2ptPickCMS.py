f08feaf08e7e1f37808b0bc57f5059d2adf9d4e3
Jack Dragos <jack.dragos@gmail.com>
2016-10-21 15:38:07 +1030

    autocommit

--- Try2ptPickCMS.py
+++ Try2ptPickCMS.py
@@ -91,6 +91,7 @@ def CreateTwoPt(thisMomList,thisSmearList,feedin= {'anaproc':AnaProc}):
         SetList += ['tsrc'+tsrc +iset for iset in CreateMassSet(thisSmearList,StateSet,[],flipord=True)]
     if len(DefSmearList) > 1: SetList += CreateMassSet([],CMStateSet,thisCMTvarList,flipord=True)
     SetList += CreateMassSet([],StateSet,thisPoFTvarList,flipord=True)
+    print SetList
     PrintCfunToFile([C2out],SetList,thisMomList,['twopt'],AddDict=InfoDict)
     PrintSetToFile([C2out],SetList,thisMomList,['Mass'],0,AddDict=InfoDict)
     print 'Printing took ' , str(datetime.timedelta(seconds=time.time()-start)) , ' h:m:s  '
