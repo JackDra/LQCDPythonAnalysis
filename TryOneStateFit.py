30974a7926869b8ace7c049141f867eb7f67d99a
Jack Dragos <jack.dragos@gmail.com>
2016-10-21 18:13:10 +1030

    autocommit

--- TryOneStateFit.py
+++ TryOneStateFit.py
@@ -177,6 +177,7 @@ if os.path.isfile(picklefile2pt):
     print '2 point picked file read in'        
 else:
     print 'Reading and fitting 2 point correlator data'
+    print ReadSetList
     [dump,data2pt,twoptGammaMomList,dump3,dump4,infolist2pt] = ReadCfunsnp(['twopt'],ReadSetList,thisMomList=feedin['mom'])
     ## data2pt = [ ip , iset2pt , it ] = bootstrap1 class (.Avg, .Std, .values, .nboot)
     OSF2ptarray = []
