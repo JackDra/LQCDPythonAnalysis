59dd3ba6164a3943e2e7e9133cc57d8674be52d2
Jack Dragos <jack.dragos@gmail.com>
2016-10-21 14:55:35 +1030

    autocommit

--- Params.py
+++ Params.py
@@ -343,8 +343,8 @@ REvecFlagList = [PickedStateStr+iREvec for iREvec in REvecTvarList]
 
 # REvecTvarList = []
 # DefPoFVarList = [17,2]
-DefPoFVarList = [18,2]
-# DefPoFVarList = [22,2]
+# DefPoFVarList = [18,2]
+DefPoFVarList = [22,2]
 AllPoFTSinkList = {'12104':[],'12090':[26,27]}
 PoFTSinkList = AllPoFTSinkList[str(kappa)]
 PoFTSinkStrList = ['tsink'+str(its) for its in PoFTSinkList]
