e24d9dafecb00042fcff8f71ef5f5a0117bde73b
Jack Dragos <jack.dragos@gmail.com>
2016-10-24 20:32:24 +1030

    autocommit

--- Params.py
+++ Params.py
@@ -122,7 +122,7 @@ else:
     CfunConfigCheck = False # Checks all two and three point correlators before running (turn to False if doing 2-pt corr analysis)
 
 ##DEBUG toggles (True/False):
-Debug = False # for debugging, toggles alot of print statements on
+Debug = True # for debugging, toggles alot of print statements on
 ScaleByP4g4 = False ## scales out all operators by P4g4 instead of 2 point correlator at tsink for Ratio value (RF)
 ShowConfNum = Debug # debugging, show number of configs during read
 PrintRead = not DoMulticore # Screws up output if on and doing mulitcore reading
