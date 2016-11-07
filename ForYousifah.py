e403471a207c9be7d3caaf8480c0b6f36023e66c
Jack Dragos <jack.dragos@gmail.com>
2016-11-07 17:33:01 -0500

    autocommit

--- ForYousifah.py
+++ ForYousifah.py
@@ -11,28 +11,30 @@ SeekIncSize = 8
 
 def ReadCfun(thisfile):
     data = []
-    with open(thisfile,'rb') as f:
-        ip,iploc = 0,qstrTOip('q = 0 0 0')
-        for it in range(nt):
-            ##11 + 22 projected spin component ns = 4 works
-            loc = SeekIncSize*2*(ns**2)*(it + iploc*nt)
-            f.seek(loc)
-            tmpdata = array('d')
-            tmpdata.read(f,1)
-            tmpdata.byteswap()
-            val = tmpdata[0]
-            f.seek(loc+10*SeekIncSize)
-            tmpdata = array('d')
-            tmpdata.read(f,1)
-            tmpdata.byteswap()
-            val += tmpdata[0]
-            data.append(val)
-            if np.isnan(data[it]):
-                raise NaNCfunError('NaN Values: ' +qvecSet[iploc] + ' it='+str(it+1) )
-            # if 'cmplx' in RI:
-            #     datai[ipread].append(tmpdata[loc+1]+tmpdata[loc+11])
-        if data[tsource-1] < 0.0:
-            data = np.negative(data)
+    f = open(thisfile,'rb')
+    # with open(thisfile,'rb') as f:
+    ip,iploc = 0,qstrTOip('q = 0 0 0')
+    for it in range(nt):
+        ##11 + 22 projected spin component ns = 4 works
+        loc = SeekIncSize*2*(ns**2)*(it + iploc*nt)
+        f.seek(loc)
+        tmpdata = array('d')
+        tmpdata.read(f,1)
+        tmpdata.byteswap()
+        val = tmpdata[0]
+        f.seek(loc+10*SeekIncSize)
+        tmpdata = array('d')
+        tmpdata.read(f,1)
+        tmpdata.byteswap()
+        val += tmpdata[0]
+        data.append(val)
+        if np.isnan(data[it]):
+            raise NaNCfunError('NaN Values: ' +qvecSet[iploc] + ' it='+str(it+1) )
+        # if 'cmplx' in RI:
+        #     datai[ipread].append(tmpdata[loc+1]+tmpdata[loc+11])
+    if data[tsource-1] < 0.0:
+        data = np.negative(data)
+    f.close()
     return data
 
 def WriteCfun(thisfile,data):
