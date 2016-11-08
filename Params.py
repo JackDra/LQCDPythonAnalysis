801c3beb9cba2185c7297ba84919573b203e6ac7
Jack Dragos <jack.dragos@gmail.com>
2016-11-08 12:28:02 -0500

    autocommit

--- Params.py
+++ Params.py
@@ -113,7 +113,7 @@ DoSym = True # symmetrises the 2 point function (see CMSTech.py)
 # VarMethodMethod = 'Symmetric' ## Symmetic matrix construction WITHOUT symmetric solver
 VarMethodMethod = 'AxBxlSolve' ## solve Ax = Bxc system directly (generalised eigenvalue problem).
 NoSFRfacScale = False # Turn on to only scale the R function by sqrt((Epp+m)(Ep+m)/EppEp) for form factor creation
-PoFDelta = 2 ## sets the PoF detla time shift parameter.
+PoFDelta = 1 ## sets the PoF detla time shift parameter.
 ReadPoF2pt = False # Create PoF using already calculated eigenvectors. This is used if the statistics or solver method has changed.
 DeCorrPoF = False ## used for debugging the pencil of function method (decorrelation problem) !!!!!DEPRECIATED, LEAVE FALSE!!!!!
 TimeInv = False ## uses time invariance to calculate the Pencil of Function method/ Oposed to calculating [tsource,tsource-1,...,tsource-PoFShifts]
