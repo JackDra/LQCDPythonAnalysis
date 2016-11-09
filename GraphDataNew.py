47bc0a6dca46607af5a8d3115771df3116381e7f
Jack Dragos <jack.dragos@gmail.com>
2016-11-09 15:51:32 -0500

    autocommit

--- GraphDataNew.py
+++ GraphDataNew.py
@@ -619,6 +619,11 @@ def PlotFFSet(dataset,thisFF,thisSetFlag,thisCurr,thisDSCurr,graphparams):
     for thisset in thisSetFlag:
         ##make legend formatting function
         thiskappa,keyset = SplitKappa(thisset)
+        if Debug:
+            print dataset.keys(), keyset
+            if keyset in dataset.keys(): dataset[keyset],thisFF
+            print
+            
         if not CheckDict(dataset,keyset,thisFF): continue
         if dataset[keyset][thisFF] == False: continue        
         thiscol = thiscolcyc.next()
