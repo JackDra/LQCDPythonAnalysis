cfbee318ff5b59d05b5c7de585bce0e59956d618
Jack Dragos <jack.dragos@gmail.com>
2016-11-09 15:48:26 -0500

    autocommit

--- GraphFFs.py
+++ GraphFFs.py
@@ -94,7 +94,7 @@ def PickFFFewSets(currdata,thiscurr,thisSetList):
     # PickedSetList += FlagList(thisSetList,'SumMeth',SumFitRPicked,SumCutPar)
     # PickedSetList += FlagList(thisSetList,'OSF',OSFCutPicked)
     # PickedSetList += FlagList(thisSetList,'TSFTsink',TSFCutPicked)
-    if DEBUG: print PickedSetList
+    if Debug: print PickedSetList
     return PickedSetList
 
 def ReadAndPlotFF(thisCurrDict,DoList='All'):
