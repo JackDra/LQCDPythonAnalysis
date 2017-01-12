c3246283adcf94ad52ce0ce69d1bb9995917d3f1
Jack Dragos <jack.dragos@gmail.com>
2017-01-12 13:36:17 -0500

    eh?

--- Params.py
+++ Params.py
@@ -143,7 +143,7 @@ XSrcLen = 20
 NoXAvg = False ## Does each source separatly for each 
 
 ForceMinXSrcLen = True ## only calculates with a minumum of MinXSRCLen of sources per gauge field
-MinXSrcLen = 15
+MinXSrcLen = 20
 ##DEBUG toggles (True/False):
 # Debug = True # for debugging, toggles alot of print statements on
 # DEBUGPoF = True ## Temp debug parameter to test PoF (REMOVE ONCE DONE)
