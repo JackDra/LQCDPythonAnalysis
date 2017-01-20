d53c8180f259f8dfc6d685e976e582660d19db78
Jack Dragos <jack.dragos@gmail.com>
2017-01-19 22:02:54 -0500

    dipole fit should be present now

--- FitParams.py
+++ FitParams.py
@@ -156,7 +156,7 @@ StateParList = {'Two':TwoStateParList,'One':OneStateParList}
 MaxIters = 100000
 def FitDefGuess(Fun,Len=1):
     if Fun.__name__ == 'DPfitfun':
-        return [1,1.6]
+        return [2,0.5]
         # return [-1,-1.6]
     if Fun.__name__ == 'DPfitfunOnePar':
         return [1.6]
