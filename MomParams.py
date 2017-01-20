abf34382f4f9b717a1fef76514deacaec0084724
Jack Dragos <jack.dragos@gmail.com>
2017-01-19 23:09:25 -0500

    dipole fit should be present now

--- MomParams.py
+++ MomParams.py
@@ -13,7 +13,8 @@ Maxqsqrd = np.max(np.array(MomSqrdSet).astype(int))
 nxyz = 32
 qunit = (2.0*np.pi)/float(nxyz)
 hbarc = 0.1973269718 ## In GeV * fermi
-latspace = 0.074 ## In fermi
+# latspace = 0.074 ## In fermi
+latspace = 0.0957 ## In fermi
 
 hbarcdivlat = hbarc/latspace
 qunitPhys = qunit*hbarcdivlat
