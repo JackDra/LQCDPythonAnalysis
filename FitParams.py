3e365704b9568532f7d0575d92ff45e7d0a0c26c
Jack Dragos <jack.dragos@gmail.com>
2017-01-19 22:33:40 -0500

    dipole fit should be present now

--- FitParams.py
+++ FitParams.py
@@ -153,7 +153,7 @@ TSFfitr = str(TSFfitvals[0])+'-'+str(TSFfitvals[1])
 
 StateParList = {'Two':TwoStateParList,'One':OneStateParList}
 
-MaxIters = 1000000
+MaxIters = 100000000
 def FitDefGuess(Fun,Len=1):
     if Fun.__name__ == 'DPfitfun':
         return [2.7,1]
