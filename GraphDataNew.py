745d8851fed8de48cc92a82fca7b603866af9cf1
Jack Dragos <jack.dragos@gmail.com>
2017-02-03 14:17:28 -0500

    Formatting Graphs

--- GraphDataNew.py
+++ GraphDataNew.py
@@ -1565,6 +1565,7 @@ def GraphchitKappas(Qlist,flowlist):
     # chitStd = coeff*0.25*Qstd*Qavg**(0.25-1)
     # pl.errorbar(flowlist+0.1,chitAvg,chitStd,fmt='o',label=r'$No Boot$')
     pl.xlim(0,pl.xlim()[1])
+    pl.ylim(0,1.5)
     pl.xlabel(r'$ m_{\pi} $')
     pl.ylabel(r'$\chi_{t}^{1/4}$')
     # pl.ylim(0,0.4)
