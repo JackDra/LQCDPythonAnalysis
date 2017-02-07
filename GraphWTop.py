391741618bee8980fcf27cc00c96e7f514cee081
Jack Dragos <jack.dragos@gmail.com>
2017-02-07 17:12:39 -0500

    GraphWTop testing

--- GraphWTop.py
+++ GraphWTop.py
@@ -19,19 +19,20 @@ from GraphDataNew import *
 ## tflow [ icfg , itflow ]
 ## topcharge [ icfg , itflow ]
 kappatopc,kappatflow = [],[]
-for ikappa in kappalist[1]:    
-    print 'Reading kappa=', ikappa
-    if Debug: print WeinDir.replace('Kud0'+str(kappa),ikappa.replace('k','Kud0'))
-    filelist,topcharge,tflow = ReadTopAll(WeinDir.replace('Kud0'+str(kappa),ikappa.replace('k','Kud0')))
-    if ikappa == kappa:
-        GraphWExp(topcharge,tflow[0])
-        GraphWLines(topcharge,tflow[0],np.arange(0,200,33))
-        GraphWchit(topcharge,tflow[0])
-        GraphW2Hist(topcharge,tflow[0].tolist().index(3.01))
+
+ikappa = kappalist[1]
+print 'Reading kappa=', ikappa
+if Debug: print WeinDir.replace('Kud0'+str(kappa),ikappa.replace('k','Kud0'))
+filelist,topcharge,tflow = ReadTopAll(WeinDir.replace('Kud0'+str(kappa),ikappa.replace('k','Kud0')))
+if ikappa == kappa:
+    GraphWExp(topcharge,tflow[0])
+    GraphWLines(topcharge,tflow[0],np.arange(0,200,33))
+    GraphWchit(topcharge,tflow[0])
+    GraphW2Hist(topcharge,tflow[0].tolist().index(3.01))
 
 
-    kappatopc.append(topcharge)
-    kappatflow.append(tflow[0])
+kappatopc.append(topcharge)
+kappatflow.append(tflow[0])
     
 
 # GraphWchitKappas(kappatopc,kappatflow)
