8f329e50f0e340ff87f423dcb5b6c442e374c141
Jack Dragos <jack.dragos@gmail.com>
2017-02-10 12:37:31 -0500

    auto errors testing

--- MiscFuns.py
+++ MiscFuns.py
@@ -84,7 +84,7 @@ def GammaAlpha_estimate(gQ,gN):
    ## equation (41)
    tau = np.array(CaW) / (2*Gat[0])
    Wopp = gW(tau)
-   return np.array(tau),CaW/float(glen),Gat,Wopp, np.array(VarTau(tau))
+   return np.array(tau),np.abs(CaW/float(glen)),Gat,Wopp, np.array(VarTau(tau))
 
 
 
