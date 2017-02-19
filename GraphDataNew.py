dd47c7e014072a03403fce418eca36a251e853e1
Jack Dragos <jack.dragos@gmail.com>
2017-02-19 16:13:32 -0500

    fixing legend for chit

--- GraphDataNew.py
+++ GraphDataNew.py
@@ -107,7 +107,8 @@ leglocFFDict = {'NeutronVectorFF2':'upper left',
                 'IsoVectorPsVectorFF2':'upper right',
                 'NeutronGeGmFF1/F1divF2':'upper left',
                 'ProtonTensorFF3':'upper left',
-                'NeutronVectorTopFF3':'lower right',
+                # 'NeutronVectorTopFF3':'lower right',
+                'NeutronVectorTopFF3':'upper right',
                 'NeutronTensorFF1':'upper left',
                 'NeutronTensorFF2':'upper left'}
 
