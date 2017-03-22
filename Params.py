0ec160fd4830e3e5f3893d858662aabb9106e4fc
Jack Dragos <jack.dragos@gmail.com>
2017-03-22 11:24:09 -0400

    autocommit

--- Params.py
+++ Params.py
@@ -165,13 +165,13 @@ MonteFlow = tflowlist.index(400)
 
 ExactXSrcNumber = False ## makes it so there are the same number of sources for each configuration (hardwired to the first configuration found)
 ForceXSrcLen = True ## forces so that any more sorce locations after XSrcLen are ignored
-XSrcLen = 20
+XSrcLen = 2
 
 ForceNConf = True
 NConfLen = 100
 
 ForceMinXSrcLen = True ## only calculates with a minumum of MinXSRCLen of sources per gauge field
-MinXSrcLen = 20
+MinXSrcLen = 2
 ##DEBUG toggles (True/False):
 # Debug = True # for debugging, toggles alot of print statements on
 # DEBUGPoF = True ## Temp debug parameter to test PoF (REMOVE ONCE DONE)
