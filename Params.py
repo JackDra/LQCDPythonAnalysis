832d2184fb692aaf54db7311340a1e942e9b66b1
Jack Dragos <jack.dragos@gmail.com>
2017-03-22 11:52:38 -0400

    heh?

--- Params.py
+++ Params.py
@@ -165,10 +165,10 @@ MonteFlow = tflowlist.index(400)
 
 ExactXSrcNumber = False ## makes it so there are the same number of sources for each configuration (hardwired to the first configuration found)
 ForceXSrcLen = True ## forces so that any more sorce locations after XSrcLen are ignored
-XSrcLen = 2
+XSrcLen = 20
 
 ForceNConf = True
-NConfLen = 5
+NConfLen = 100
 
 ForceMinXSrcLen = True ## only calculates with a minumum of MinXSRCLen of sources per gauge field
 MinXSrcLen = 2
@@ -274,7 +274,7 @@ elif 'ReadSet' in ListOrSet:
         if 'JackLappy' in socket.gethostname():
             nboot = 20
         else:
-            nboot = 2
+            nboot = 200
 
 print 'Boot Size =', nboot , ', Folder Location:', ListOrSet, ', AverageX =',XAvg
 # tsource = 17
