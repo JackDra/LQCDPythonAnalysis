94fdcede60be7c51abb97085e56b41d8773c5d26
Jack Dragos <jack.dragos@gmail.com>
2017-03-23 14:52:34 -0400

    Debugging easier now

--- Params.py
+++ Params.py
@@ -521,7 +521,7 @@ else:
         DefPoFVarPicked = [[4,3]]
     else:
         # DefPoFVarPicked = [[3,3],[1,3],[2,3],[4,3]]
-        DefPoFVarPicked = [[3,3],[1,3]] ## temporary, TODO FIX, should work
+        DefPoFVarPicked = [[1,3],[4,3]] ## temporary, TODO FIX, should work
 
 DefPoFTvarRef = DefPoFVarPicked[0]
 PoFTvarPicked = ['PoF'+str(PoFShifts)+'to'+str(iPoF[0])+'dt'+str(iPoF[1]) for iPoF in DefPoFVarPicked]
