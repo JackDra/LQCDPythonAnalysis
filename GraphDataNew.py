f323bb05df6122d7d3e7fc30c8d6d4ccf46704e1
Jack Dragos <jack.dragos@gmail.com>
2017-03-27 09:41:01 -0400

    autocommit

--- GraphDataNew.py
+++ GraphDataNew.py
@@ -95,8 +95,8 @@ leglocDict = {'ProtonP4g4':'upper left'}
 #               'NeutronGeGmFF1/F1divF2':[0.0,0.06]}
 # ylimFFDict = {'ProtonVectorTopFF3':[-0.15,0.25],'NeutronVectorTopFF3':[-0.15,0.25],'PandNVectorTopFF3':[-0.15,0.25],
 #               'ProtonVectorWeinFF3':[-20,15]}
-ylimFFDict = {'ProtonVectorTopFF3':[-0.05,0.35],'NeutronVectorTopFF3':[-0.25,0],'PandNVectorTopFF3':[-.2,.3],
-              'ProtonVectorWeinFF3':[-20,15]}
+# ylimFFDict = {'ProtonVectorTopFF3':[-0.05,0.35],'NeutronVectorTopFF3':[-0.25,0],'PandNVectorTopFF3':[-.2,.3],
+#               'ProtonVectorWeinFF3':[-20,15]}
 
 leglengthFFDict = {'ProtonVectorTopFF3':2,'NeutronVectorTopFF3':2,'PandNVectorTopFF3':2,
                    'ProtonVectorWeinFF3':2,'NeutronVectorWeinFF3':2,'PandNVectorWeinFF3':1}
