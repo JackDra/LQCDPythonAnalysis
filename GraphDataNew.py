ef17ec767aad9df031e761a4174736b9e07fb690
Jack Dragos <jack.dragos@gmail.com>
2017-04-04 15:41:45 -0400

    autocommit

--- GraphDataNew.py
+++ GraphDataNew.py
@@ -2045,8 +2045,10 @@ def GraphWchitKappasOverFlow(Wlist,flowlist,thiskappalist):
         # chitdivlogErr = Pullflag(chit,'Std')/np.log(np.array(iflowlist))
         # chitdivlog = Pullflag(chit,'Avg')/np.log(TflowToPhys(iflowlist))
         # chitdivlogErr = Pullflag(chit,'Std')/np.log(TflowToPhys(iflowlist))
-        chitdivlog = Pullflag(chit,'Avg')/np.log(np.array(TflowToPhys(iflowlist))+2)
-        chitdivlogErr = Pullflag(chit,'Std')/np.log(np.array(TflowToPhys(iflowlist))+2)
+        # chitdivlog = Pullflag(chit,'Avg')/np.log(np.array(TflowToPhys(iflowlist))+2)
+        # chitdivlogErr = Pullflag(chit,'Std')/np.log(np.array(TflowToPhys(iflowlist))+2)
+        chitdivlog = Pullflag(chit,'Avg')*np.log(np.array(TflowToPhys(iflowlist))+2)
+        chitdivlogErr = Pullflag(chit,'Std')*np.log(np.array(TflowToPhys(iflowlist))+2)
         pl.errorbar(TflowToPhys(iflowlist+ishift),chitdivlog,chitdivlogErr,fmt='o',color=thiscol,alpha=0.5,label=r'$ \chi / log(\sqrt{8t_{f}})$')
 
     # Qavg = np.mean(np.array(Wlist)**2,axis=0)
