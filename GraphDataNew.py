ed9ed4d5954f71d3a2c76fc05c65ecdfcc7b2dfb
Jack Dragos <jack.dragos@gmail.com>
2017-04-05 17:03:10 -0400

    autocommit

--- GraphDataNew.py
+++ GraphDataNew.py
@@ -166,48 +166,46 @@ DatFile = False
     #         PlotExp('NeutronEDM',thiscolcyc)
 
 CRadConv = 1/(6*hbarc**2)
+ExpxvalsCrad = np.array([0,0.05])
     
 def PlotExp(flag,thiscolcyc):
     if 'ChargeRadius' in flag:
-        xvals = np.array([0,0.1])
-        yvals = 1-CRadConv*ExpValues['CRad_muon'][0]*xvals
-        yup,ydown = 1-CRadConv*np.sum(ExpValues['CRad_muon'])*xvals,1-CRadConv*(ExpValues['CRad_muon'][0]-ExpValues['CRad_muon'][1])*xvals
+        yvals = 1-CRadConv*ExpValues['CRad_muon'][0]*ExpxvalsCrad
+        yup,ydown = 1-CRadConv*np.sum(ExpValues['CRad_muon'])*ExpxvalsCrad,1-CRadConv*(ExpValues['CRad_muon'][0]-ExpValues['CRad_muon'][1])*ExpxvalsCrad
         thiscol = thiscolcyc.next()
-        pl.plot(xvals,yvals,color=thiscol,label=r'ANTOGNINI 13, $\mu p-atom$ Lamb shift $\langle r^2 \rangle='+MakeValAndErr(*np.abs(ExpValues['CRad_muon']))+'\ fm^{2}$')
-        pl.fill_between(xvals,yup,ydown,color=thiscol,alpha=thisalpha,edgecolor='none')
+        pl.plot(ExpxvalsCrad,yvals,color=thiscol,label=r'ANTOGNINI 13, $\mu p-atom$ Lamb shift $\langle r^2 \rangle='+MakeValAndErr(*np.abs(ExpValues['CRad_muon']))+'\ fm^{2}$')
+        pl.fill_between(ExpxvalsCrad,yup,ydown,color=thiscol,alpha=thisalpha,edgecolor='none')
         thiscol = thiscolcyc.next()
-        yvals = 1-CRadConv*ExpValues['CRad_electron'][0]*xvals
-        yup,ydown = 1-CRadConv*np.sum(ExpValues['CRad_electron'])*xvals,1-CRadConv*(ExpValues['CRad_electron'][0]-ExpValues['CRad_electron'][1])*xvals
-        pl.plot(xvals,yvals,color=thiscol,label=r'MOHR 12, 2010 CODATA $e p$ data $\langle r^2 \rangle='+MakeValAndErr(*np.abs(ExpValues['CRad_electron']))+'\ fm^{2}$')
-        pl.fill_between(xvals,yup,ydown,color=thiscol,alpha=thisalpha,edgecolor='none')
+        yvals = 1-CRadConv*ExpValues['CRad_electron'][0]*ExpxvalsCrad
+        yup,ydown = 1-CRadConv*np.sum(ExpValues['CRad_electron'])*ExpxvalsCrad,1-CRadConv*(ExpValues['CRad_electron'][0]-ExpValues['CRad_electron'][1])*ExpxvalsCrad
+        pl.plot(ExpxvalsCrad,yvals,color=thiscol,label=r'MOHR 12, 2010 CODATA $e p$ data $\langle r^2 \rangle='+MakeValAndErr(*np.abs(ExpValues['CRad_electron']))+'\ fm^{2}$')
+        pl.fill_between(ExpxvalsCrad,yup,ydown,color=thiscol,alpha=thisalpha,edgecolor='none')
     # if 'NeutronCRad' in flag:
-    #     xvals = np.array([0,0.1])
-    #     yvals = CRadConv*ExpValues['NeutronCRad'][0]*xvals
-    #     yup,ydown = CRadConv*np.sum(ExpValues['NeutronCRad'])*xvals,CRadConv*(ExpValues['NeutronCRad'][0]-ExpValues['NeutronCRad'][1])*xvals
+    #     ExpxvalsCrad = np.array([0,0.1])
+    #     yvals = CRadConv*ExpValues['NeutronCRad'][0]*ExpxvalsCrad
+    #     yup,ydown = CRadConv*np.sum(ExpValues['NeutronCRad'])*ExpxvalsCrad,CRadConv*(ExpValues['NeutronCRad'][0]-ExpValues['NeutronCRad'][1])*ExpxvalsCrad
     #     thiscol = thiscolcyc.next()
-    #     pl.plot(xvals,yvals,color=thiscol,label=r'$ne$ scattering , pdg average $\langle r_{n}^2 \rangle='+MakeValAndErr(*np.abs(ExpValues['NeutronCRad']))+'\ fm^{2}$')
-    #     pl.fill_between(xvals,yup,ydown,color=thiscol,alpha=thisalpha,edgecolor='none')
+    #     pl.plot(ExpxvalsCrad,yvals,color=thiscol,label=r'$ne$ scattering , pdg average $\langle r_{n}^2 \rangle='+MakeValAndErr(*np.abs(ExpValues['NeutronCRad']))+'\ fm^{2}$')
+    #     pl.fill_between(ExpxvalsCrad,yup,ydown,color=thiscol,alpha=thisalpha,edgecolor='none')
     if 'NeutronMagMom' in flag:
         pl.errorbar([0.0],[ExpValues['MagMomNeutron'][0]],[ExpValues['MagMomNeutron'][1]],fmt='x',color=thiscolcyc.next(),label='2010 CODATA, $\mu_{n}='+MakeValAndErr(*ExpValues['MagMomNeutron'])+'$')
     if 'ProtonMagMom' in flag:
         pl.errorbar([0.0],[ExpValues['MagMomProton'][0]],[ExpValues['MagMomProton'][1]],fmt='x',color=thiscolcyc.next(),label='2010 CODATA, $\mu_{p}='+MakeValAndErr(*ExpValues['MagMomProton'])+'$')
     if 'MRadProton' in flag:
-        xvals = np.array([0,0.1])
-        yvals = ExpValues['MagMomProton'][0]-CRadConv*ExpValues['MRadProton'][0]*xvals*ExpValues['MagMomProton'][0]
-        yup,ydown = (ExpValues['MagMomProton'][0]-CRadConv*np.sum(ExpValues['MRadProton'])*xvals*ExpValues['MagMomProton'][0],
-                     ExpValues['MagMomProton'][0]-CRadConv*(ExpValues['MRadProton'][0]-ExpValues['MRadProton'][1])*xvals*ExpValues['MagMomProton'][0])
+        yvals = ExpValues['MagMomProton'][0]-CRadConv*ExpValues['MRadProton'][0]*ExpxvalsCrad*ExpValues['MagMomProton'][0]
+        yup,ydown = (ExpValues['MagMomProton'][0]-CRadConv*np.sum(ExpValues['MRadProton'])*ExpxvalsCrad*ExpValues['MagMomProton'][0],
+                     ExpValues['MagMomProton'][0]-CRadConv*(ExpValues['MRadProton'][0]-ExpValues['MRadProton'][1])*ExpxvalsCrad*ExpValues['MagMomProton'][0])
         thiscol = thiscolcyc.next()
-        pl.plot(xvals,yvals,color=thiscol,label=r'BELUSHKIN 07 Dispersion Analysis $\langle r_{\mu}^2 \rangle='+MakeValAndErr(*np.abs(ExpValues['MRadProton']))+'\ fm^{2}$')
-        pl.fill_between(xvals,yup,ydown,color=thiscol,alpha=thisalpha,edgecolor='none')
+        pl.plot(ExpxvalsCrad,yvals,color=thiscol,label=r'BELUSHKIN 07 Dispersion Analysis $\langle r_{\mu}^2 \rangle='+MakeValAndErr(*np.abs(ExpValues['MRadProton']))+'\ fm^{2}$')
+        pl.fill_between(ExpxvalsCrad,yup,ydown,color=thiscol,alpha=thisalpha,edgecolor='none')
     if 'MRadNeutron' in flag:
-        xvals = np.array([0,0.1])
-        yvals = ExpValues['MagMomNeutron'][0]-CRadConv*ExpValues['MRadNeutron'][0]*xvals*np.abs(ExpValues['MagMomNeutron'][0])
-        yup,ydown = (ExpValues['MagMomNeutron'][0]-CRadConv*np.sum(ExpValues['MRadNeutron'])*xvals*np.abs(ExpValues['MagMomNeutron'][0]),
-                     ExpValues['MagMomNeutron'][0]-CRadConv*(ExpValues['MRadNeutron'][0]-ExpValues['MRadNeutron'][1])*xvals*np.abs(ExpValues['MagMomNeutron'][0]))
+        yvals = ExpValues['MagMomNeutron'][0]-CRadConv*ExpValues['MRadNeutron'][0]*ExpxvalsCrad*np.abs(ExpValues['MagMomNeutron'][0])
+        yup,ydown = (ExpValues['MagMomNeutron'][0]-CRadConv*np.sum(ExpValues['MRadNeutron'])*ExpxvalsCrad*np.abs(ExpValues['MagMomNeutron'][0]),
+                     ExpValues['MagMomNeutron'][0]-CRadConv*(ExpValues['MRadNeutron'][0]-ExpValues['MRadNeutron'][1])*ExpxvalsCrad*np.abs(ExpValues['MagMomNeutron'][0]))
         thiscol = thiscolcyc.next()
         thisleg = r'BELUSHKIN 07 Dispersion Analysis $\langle r_{\mu}^2 \rangle='+MakeValAndErr(*np.abs(ExpValues['MRadNeutron']))+'\ fm^{2}$'
-        pl.plot(xvals,yvals,color=thiscol,label=thisleg)
-        pl.fill_between(xvals,yup,ydown,color=thiscol,alpha=thisalpha,edgecolor='none')
+        pl.plot(ExpxvalsCrad,yvals,color=thiscol,label=thisleg)
+        pl.fill_between(ExpxvalsCrad,yup,ydown,color=thiscol,alpha=thisalpha,edgecolor='none')
     # if 'ProtonEDM' in flag or 'PandNEDM' in flag:
     #     thiscol = thiscolcyc.next()
     #     thislab = r'$ d_{p} < '+r'{:0.2e} \theta fm$'.format(ExpValues['ProtonEDMtfm'])
