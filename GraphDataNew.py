6436d98d2bc978d876fa8103d4d690ce8ad82b5e
Jack Dragos <jack.dragos@gmail.com>
2017-04-04 23:54:39 -0400

    autocommit

--- GraphDataNew.py
+++ GraphDataNew.py
@@ -2098,6 +2098,7 @@ def GraphWchitKappasOverFlow(Wlist,flowlist,thiskappalist):
     # pl.ylabel(r'$\frac{\chi_{t}^{1/8}}{ log\left(100*\sqrt{8t_{f}}\right)} GeV$')
     pl.ylabel(r'$\frac{\chi_{t}^{1/8}}{ log\left(\sqrt{8t_{f}}+2\right)} GeV$')
     # pl.ylim(0,0.4)
+    pl.legend()
     # pl.legend(loc='lower right')
     thisdir = outputdir[0] + 'graphs/Wdata/'
     pl.title(r'$ \chi_{t}^{1/8} = \frac{1}{V^{1/8}} \langle W^2 \rangle^{1/8} $',y=TitleShift)
