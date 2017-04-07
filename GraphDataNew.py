15ef63507bcb82f7e3e1fbeb3df11a0f58c022a1
Jack Dragos <jack.dragos@gmail.com>
2017-04-06 20:46:45 -0400

    autocommit

--- GraphDataNew.py
+++ GraphDataNew.py
@@ -355,7 +355,7 @@ def SetFFAxies(thisCurr):
     currnumb = thisCurr[-1]
     pl.xlabel(FFxlab)
     if currnumb == '3':
-        pl.ylabel(FFylab.replace('F','\\frac{F_{'+currnumb+'}').replace(') $',')}{2m_{N,phys}} (\theta fm) $'))
+        pl.ylabel(FFylab.replace('F','\\frac{F_{'+currnumb+'}').replace(') $',r')}{2m_{N,phys}} (\theta fm) $'))
     else:
         if 'GeGm' in thisCurr:
             if currnumb == '1':
